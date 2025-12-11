ea48d039289311ac01eadfe5cbf1f7292c093636
r akhil raj <158182510+akima6@users.noreply.github.com>
2025-12-12 00:15:09 +0530

    Update trainer.py

--- generative_agent/ppo_trainer/trainer.py
+++ generative_agent/ppo_trainer/trainer.py
@@ -15,7 +15,8 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, "../.."))
 
 # crystalformer package lives here:
-/# /content/the-generative-agent/CrystalFormer/crystalformer
+
+# /content/the-generative-agent/CrystalFormer/crystalformer
 CRYSTALFORMER_DIR = os.path.join(PROJECT_ROOT, "CrystalFormer")
 
 # Ensure both project root and CrystalFormer path are importable
